e470cf6b53a42ce2bd1511b9204813037057ca50
silead <80070723+silead@users.noreply.github.com>
2021-03-12 20:15:42 +0300

    last1

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Look : MonoBehaviour
+{
+    RaycastHit hit;
+    Ray ray;
+    [SerializeField] private Image _stayPoint;
+    [SerializeField] private Image _canPoint;
+
+    private void Start()
+    {
+        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 10f))
+        {
+            if (hit.transform.GetComponent<Teleport>() != null)
+            {
+                _stayPoint.enabled = false;
+                _canPoint.enabled = true;
+            }
+            else
+            {
+                _stayPoint.enabled = true;
+                _canPoint.enabled = false;
+            }
+        }
+        else
+        {
+            _stayPoint.enabled = true;
+            _canPoint.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    RaycastHit hit;
+    Ray ray;
+    RaycastHit hitObj;
+    bool isKinematic;
+
+
+    bool b;
+    private float _rayDistance = 10f;
+
+    private void Start()
+    {
+        if (gameObject.GetComponent<Rigidbody>().isKinematic == true)
+        {
+            isKinematic = true;
+        }
+    }
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (b == true)
+        {
+            Camera.main.transform.parent.transform.position = Vector3.MoveTowards(Camera.main.transform.parent.transform.position, gameObject.transform.position, Time.deltaTime * 30);
+            //надо выключить 
+            //Debug.Log(gameObject.transform.position - Camera.main.transform.parent.transform.position);
+            if (gameObject.transform.position - Camera.main.transform.parent.transform.position == new Vector3(0, 0, 0))
+            {
+                b = false;
+                Camera.main.transform.parent.rotation = new Quaternion(0, 0, 0, 0);
+                Camera.main.transform.rotation = new Quaternion(0, 0, 0, 0);
+                gameObject.transform.rotation = new Quaternion(0,0,0,0);
+                Camera.main.transform.parent.GetComponent<CapsuleCollider>().enabled = true;
+                Camera.main.transform.parent.GetComponent<Rigidbody>().isKinematic = false;
+                Camera.main.transform.parent.GetComponent<CapsuleCollider>().height = gameObject.GetComponent<CapsuleCollider>().height * gameObject.transform.localScale.y;
+                Camera.main.transform.parent.GetComponent<CapsuleCollider>().radius = gameObject.GetComponent<CapsuleCollider>().radius * gameObject.transform.localScale.x;
+                gameObject.transform.SetParent(Camera.main.transform.parent);
+                Camera.main.transform.parent.GetChild(1).GetComponent<Rigidbody>().isKinematic = true;
+                Collider[] colidermasiv = Camera.main.transform.parent.GetChild(1).GetComponents<Collider>();
+                for (int i = 0; i < colidermasiv.Length; i++)
+                {
+                    colidermasiv[i].enabled = false;
+                }
+                // pojaluist Camera.main.transform.parent.GetChild(1).GetComponent<CapsuleCollider>().enabled = false;
+                Camera.main.transform.parent.GetChild(1).gameObject.layer = 4;
+                for (int i = 0; i < Camera.main.transform.parent.GetChild(1).childCount; i++)
+                {
+                    Camera.main.transform.parent.GetChild(1).GetChild(i).gameObject.layer = 4;
+                }
+                _rayDistance = 10f;
+            }
+        }
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        
+        if (Physics.Raycast(ray, out hit, _rayDistance))
+        {
+            _rayDistance = 0f;
+            b = true;
+            Camera.main.transform.parent.GetComponent<CapsuleCollider>().enabled = false;
+            Camera.main.transform.parent.GetComponent<Rigidbody>().isKinematic = true;
+            Collider[] colliders = Camera.main.transform.parent.GetChild(1).GetComponents<Collider>();
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                colliders[i].enabled = true;
+            }
+            // не надо Camera.main.transform.parent.GetChild(1).GetComponent<CapsuleCollider>().enabled = true;
+            if (Camera.main.transform.parent.GetChild(1).gameObject.GetComponent<Teleport>().isKinematic == false)
+            {
+                Camera.main.transform.parent.GetChild(1).gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            Camera.main.transform.parent.GetChild(1).gameObject.layer = 0;
+            for (int i = 0; i < Camera.main.transform.parent.GetChild(1).childCount; i++)
+            {
+                Camera.main.transform.parent.GetChild(1).GetChild(i).gameObject.layer = 0;
+            }
+            Camera.main.transform.parent.GetChild(1).SetParent(null);
+
+            //Camera.main.gameObject.transform.SetParent(gameObject.transform.GetChild(0));
+        }
+    }
+}
